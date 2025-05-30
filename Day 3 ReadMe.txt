e5da9a675cb3fa0cd6b5e3e758f73f2cfd1668bc
Saboor Ul Fajr <hashtag.saboor@gmail.com>
2025-05-30 17:24:04 +0500

    Add files via upload

---
+++ Day 3 ReadMe.txt
@@ -0,0 +1,17 @@
+Day 3 -> Comments and scape sequence
+
+Comments in Python:
+
+. Comments are notes in the code that Python ignores; they help explain what the code does.
+
+. Single-line comment: Starts with #.
+
+. Multi-line comment: Use triple quotes ''' or """.
+
+Escape Sequences in Python:
+
+. Escape sequences are special codes in strings that do something instead of showing as text.
+
+. They Starts with a backslash \.
+
+. Common types: \n (new line), \t (tab), \\ (backslash), \" (double quote).
---
+++ Day 4 Variables and data type.py
@@ -0,0 +1,31 @@
+#Day -> Variables and data types
+
+#Variables
+number = 90           # int  
+floatingnum = 36.5   # float  
+is_coding = True     # bool  
+message = "Hello"    # str  
+z = 3 + 4j           # complex  
+y= complex(3, 4)     #complex
+
+
+#Data types
+print("The type of number is ", type(number))                     #<class 'float'>
+print("The type of is_coding is ", type(is_coding))               #<class 'bool'>       
+print("The type of message is ", type(message ))                  #<class 'str'>
+print("The type of z is ", type(z))                               #<class 'complex'>
+print("The type of y is ", type(y))                               #<class 'complex'> 
+
+#List
+fruits = ["apple", "banana", "cherry"]
+print(fruits)
+
+
+#Dictionary
+student = {"name": "Sara", "age": 18, "grade": "A"}
+print(student)
+
+
+#Tuple
+person = ("Sara", 25, "Engineer")
+print(person)
