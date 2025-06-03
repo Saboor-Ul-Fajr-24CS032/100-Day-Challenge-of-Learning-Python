b6c32c80c11098e67137af633558edbbd9454427
Saboor Ul Fajr <hashtag.saboor@gmail.com>
2025-06-03 22:11:57 +0500

    Add files via upload

---
+++ Day 12 For Loop.py
@@ -0,0 +1,31 @@
+
+
+#Day 12 -> For loops
+
+
+var= "coding!!"
+for i in var:
+    print(i)
+
+
+#nested loop
+var1=["one", "two", "three", "four"]
+for var2 in var1:
+    print(var2)
+    for var3 in var2:
+        print(var3) 
+
+# range(start:0 , end 4 )
+for var4 in range(5):
+    print(var4)
+
+
+# range(Start : 3 , end : 5)
+for var5 in range(3, 6):
+    print(var5)
+
+
+
+#range(start: 3 ,end :10, increament:2)
+for var6 in range(3, 10, 3):
+    print(var6)
