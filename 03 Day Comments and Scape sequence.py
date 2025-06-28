3ac6249bf5d9ce9b338e5b6a0322594432b2d68b
Saboor Ul Fajr <hashtag.saboor@gmail.com>
2025-06-28 16:52:12 +0500

    Add files via upload

---
+++ 03 Day Comments and Scape sequence.py
@@ -0,0 +1,20 @@
+#Day 3 -> Comments and scape sequence
+
+print("Here , I am using Comment , the one interpretor ignores ")    # Single Comments
+print("There are two types of comments single and double comments ")    
+
+''' 
+this is a  multi line comment using triple single quotes 
+'''
+
+"""
+This is a multiline comment using triple double quotes
+"""
+
+#Scape Sequence 
+
+print("There are different types of scape sequences ")
+print("this one for next line \n you can see")                          #\n (For next line)
+print("this one for Tab \t you can see")                                #\t (For tab)
+print("this one for backslash \\ you can see")                          #\\ (For backslash)
+print("this one for double quote in string \" (you can see")             #\" (For double quote)
---
+++ 06 Day ReadMe.txt
@@ -0,0 +1,13 @@
+Day 6 -> Taking input from user
+
+
+Taking User Input in Python:
+In Python, user input is taken using the input() function. It allows the user to enter data during program execution.
+
+1. Basic Input:
+When you use input(), it always takes the input as a string by default.
+
+2. Input with Type Conversion:
+If you want to take a number as input (like an integer or a float), you need to convert the input manually using typecasting.
+
+
